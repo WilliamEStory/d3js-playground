67231102bc489f834f562101188558988d8ee06b
William Story <william.story.e@gmail.com>
2026-03-03 21:56:49 -0700

    Make state control a dropdown

--- src/components/state/State.stories.tsx
+++ src/components/state/State.stories.tsx
@@ -1,22 +1,48 @@
 import type { Meta, StoryObj } from '@storybook/react-vite';
+import geoStates from '../../data/united-states-geo-json/geoStates500Kb.json';
 import { State } from "./State";
+
+const stateOptions = [...new Set(geoStates.features.map((feature) => feature.properties.NAME))].sort((a, b) =>
+  a.localeCompare(b)
+);
 
 export default {
   title: "State",
   component: State,
+  argTypes: {
+    state: {
+      control: { type: 'select' },
+      options: stateOptions,
+    },
+  },
 } satisfies Meta<typeof State>;
 
-export const Default: StoryObj<typeof State> = {
+type Story = StoryObj<typeof State>;
+
+export const Default: Story = {
   args: {
-    state: "Washington",
+    state: stateOptions[0],
     width: 400,
     height: 300,
     showLabel: true,
   }
 };
 
-export const California = () => <State state="California" />;
+export const California: Story = {
+  args: {
+    state: "California",
+  }
+};
 
-export const ByFipsCode = () => <State state="48" />;
+export const ByFipsCode: Story = {
+  args: {
+    state: "48",
+  }
+};
 
-export const WithLabel = () => <State state="Washington" showLabel />;
+export const WithLabel: Story = {
+  args: {
+    state: "Washington",
+    showLabel: true,
+  }
+};
